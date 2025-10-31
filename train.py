6275ea3475c5a2169d3984097a3eeee0d625594c
WinnieTang1 <wtang0316@gmail.com>
2025-10-31 01:43:00 -0400

    test

--- train.py
+++ train.py
@@ -193,7 +193,7 @@ def run_trainer(rank, world_size):
             model_args[k] = checkpoint_model_args[k]
         # create the model
         gptconf = GPTConfig(**model_args)
-        model = GPT(gptconf, 'ps')
+        model = GPT(gptconf)
         state_dict = checkpoint['model']
         # fix the keys of the state dictionary :(
         # honestly no idea how checkpoints sometimes get this prefix, have to debug more
@@ -204,14 +204,15 @@ def run_trainer(rank, world_size):
         model.load_state_dict(state_dict)
         iter_num = checkpoint['iter_num']
         best_val_loss = checkpoint['best_val_loss']
-    elif init_from.startswith('gpt2'):
-        print(f"Initializing from OpenAI GPT-2 weights: {init_from}")
-        # initialize from OpenAI GPT-2 weights
-        override_args = dict(dropout=dropout)
-        model = GPT.from_pretrained(init_from, override_args)
-        # read off the created config params, so we can store them into checkpoint correctly
-        for k in ['n_layer', 'n_head', 'n_embd', 'block_size', 'bias', 'vocab_size']:
-            model_args[k] = getattr(model.config, k)
+    # unneeded, not using GPT 2
+    # elif init_from.startswith('gpt2'):
+    #     print(f"Initializing from OpenAI GPT-2 weights: {init_from}")
+    #     # initialize from OpenAI GPT-2 weights
+    #     override_args = dict(dropout=dropout)
+    #     model = GPT.from_pretrained(init_from, override_args)
+    #     # read off the created config params, so we can store them into checkpoint correctly
+    #     for k in ['n_layer', 'n_head', 'n_embd', 'block_size', 'bias', 'vocab_size']:
+    #         model_args[k] = getattr(model.config, k)
     # crop down the model block size if desired, using model surgery
     if block_size < model.config.block_size:
         model.crop_block_size(block_size)
@@ -282,7 +283,7 @@ def run_trainer(rank, world_size):
     X, Y = get_batch('train') # fetch the very first batch
     t0 = time.time()
     local_iter_num = 0 # number of iterations in the lifetime of this process
-    raw_model = model.module if ddp else model # unwrap DDP container if needed
+    raw_model = model #.module if ddp else model # unwrap DDP container if needed
     running_mfu = -1.0
     while True:
 
@@ -292,7 +293,7 @@ def run_trainer(rank, world_size):
         #     param_group['lr'] = lr
 
         # evaluate the loss on train/val sets and write checkpoints
-        if iter_num % eval_interval == 0 and master_process:
+        if iter_num % eval_interval == 0: #and master_process:
             losses = estimate_loss()
             print(f"step {iter_num}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
             # if wandb_log:
@@ -369,7 +370,7 @@ def run_trainer(rank, world_size):
         t1 = time.time()
         dt = t1 - t0
         t0 = t1
-        if iter_num % log_interval == 0 and master_process:
+        if iter_num % log_interval == 0: #and master_process:
             # get loss as float. note: this is a CPU-GPU sync point
             # scale up to undo the division above, approximating the true total loss (exact would have been a sum)
             lossf = loss.item() * gradient_accumulation_steps
